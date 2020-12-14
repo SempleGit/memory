3c1a9c6a1691b1265caab6f3bd8b0ff89e02341d
Daniel Semple <dsemple.isu@gmail.com>
2020-12-13 20:39:29 -0600

    created shell and pseudocode

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React from 'react';
+import Cards from './components/Cards';
+
 function App() {
   return (
     <div className="App">
       <h1>Memory Game!</h1>
+      {/* 1. Create array of cards
+          2. Display cards in random order
+          3. Track selected card on click
+          4. Track score based on correct clicks
+          5. Identify when duplicate card is clicked
+          6. reset game */}
+      <Cards />
     </div>
   );
 }
